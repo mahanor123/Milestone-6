c6d716252fa318ab0b874df01d1df653a11832a4
Deepaksingam <55790550+Deepaksingam@users.noreply.github.com>
2019-11-20 16:06:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+const { GraphQLServer }= require('graphql-yoga')
+const mongoose= require("mongoose");
+mongoose.connect("mongodb://localhost/test");//connect to db
+const Todo = mongoose.model('Todo',{
+     text:String,
+     complete: Boolean
+});
+const typeDefs=`
+   type Query{
+        hello(name:String): String!
+        todos: [Todo]         
+   }
+   type Todo {
+        text: String!
+         id:ID! 
+         complete:Boolean!
+     }
+   type Mutation { createTodo(text: String!): Todo
+                    updateTodo(id: ID!,complete: Boolean!): Boolean
+                    removeTodo(id: ID!): Boolean
+}
+`;   //tells about the schema, !-mandatory
+const resolvers = {
+     Query:{
+            hello: (_,{name}) => `Hello ${name || "world"}`,
+            todos: () => Todo.find()
+     },
+     Mutation: {
+          createTodo: async(_,{ text }) => {
+               const todo = new Todo( { text,complete:false});
+               await todo.save();
+               return todo;
+          },
+          updateTodo:async(_,{id,complete}) =>{
+              await Todo.findByIdAndUpdate(id, {complete});
+               return true;
+          },
+          removeTodo:async(_,{id}) =>{
+               await Todo.findByIdAndRemove(id);
+                return true;
+           }
+     }
+};
+const server = new GraphQLServer({ typeDefs, resolvers });
+mongoose.connection.once("open",function(){
+     server.start(() => {
+          return console.log("sever is running on localhost:4000");
+      })     //we are connected
+});
